d7348b6edeb6f370a078edf22db04b2bd5b66753
Luan <luanraasch@outlook.com>
2022-07-06 22:00:53 -0300

    Initial commit

---
+++ src/TiposDados.java
@@ -0,0 +1,33 @@
+public class TiposDados {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        byte b2 =20;
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+        int i2 = 200000;
+        //int i1 = -10000000000000;
+        int i3 = -4000;
+
+        long l1 = 1000000000000000000L;
+        long l2 = 200000000000000000L;
+
+        float f1 = 4.5f;
+
+        double d1 = 86.89;
+        double d2 =32.98;
+
+        char c1 = 'L';
+
+        String bo1 = "Luan";
+
+        boolean bo3 = false;
+        boolean bo4 = true;
+
+        System.out.println(l1);
+
+
+
+    }
+}
---
+++ src/Variaveis.java
@@ -0,0 +1,30 @@
+
+public class Variaveis {
+     public static void main(String[] args) {
+         int numeroEstudantesAtivos;
+         
+         char nomesEstudantes;
+         
+         double mediaFinal;
+         
+         boolean vOuF;
+
+         int i = 10;
+         //variavel de controle
+         
+         final int j = 10;
+         // é uma constante
+         
+         int quantidadeProduto;
+         //cammelCase
+         
+         final int QUANTIDADE_PRODUTO;
+         //UMA CONSTANTE; é uma boa prática
+         
+         
+         
+         
+         
+         
+     }
+ }
